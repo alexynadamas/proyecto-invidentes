30a4e3411119d43f37478ca7d1425019a1176fd6
Alex <alexclementefornas95@gmail.com>
2020-05-21 17:38:25 +0200

    Se crean las interfaces gráficas

--- proyecto-invidentes/src/controlador/FXMLNavegacionController.java
+++ proyecto-invidentes/src/controlador/FXMLNavegacionController.java
@@ -3,33 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto.invidentes;
+package controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 /**
+ * FXML Controller class
  *
  * @author Alex Clemente < DAW >
  */
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+public class FXMLNavegacionController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void reproducirSiguienteInstruccion(MouseEvent event) {
+    }
+
+    @FXML
+    private void cargarVentanaPrincipal(MouseEvent event) {
+    }
+
+    @FXML
+    private void pararContinuarNavegacion(MouseEvent event) {
+    }
     
 }
